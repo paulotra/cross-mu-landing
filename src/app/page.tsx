e2bb7721dc4a4bd62f427a3fb90d3bb00d1afd31
Paulo Trajano <trajanopaulo10@gmail.com>
2026-05-05 01:16:42 +0800

    update navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+import Image from 'next/image'
+
 export default async function Home() {
   return (
     <div className="min-h-[1000px]">
       <section className="relative flex min-h-screen w-full overflow-visible">
         {/* Base background image */}
-        <img
+        <Image
           src="/home/hero.png"
           alt=""
+          width={3120}
+          height={2646}
           className="absolute inset-0 w-full object-cover object-top-right"
+          priority
         />
 
         <div className="flex max-w-1/2 flex-1 items-center justify-center">
-          <img src="/logo-emblem.png" alt="" className="relative max-w-[476px]" />
+          <Image
+            src="/logo-emblem.png"
+            alt=""
+            width={952}
+            height={952}
+            className="relative h-auto max-w-[476px]"
+          />
         </div>
 
         {/* Video overlay — blended on top of background */}
@@ -34,7 +45,13 @@ export default async function Home() {
         </video>
       </section>
       <div className="relative">
-        <img src="/bg-strip.png" alt="" className="absolute inset-0 top-[-400px] z-10 w-full" />
+        <Image
+          src="/bg-strip.png"
+          alt=""
+          width={3120}
+          height={5800}
+          className="absolute inset-0 top-[-400px] z-10 w-full"
+        />
       </div>
     </div>
   )
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,14 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <div className="flex items-center gap-x-3">
-      <Image src="/logo-icon.png" className="h-[48px] w-auto" alt="" width={74} height={96} />
-      <Image src="/logo-text.svg" className="h-[24px] w-[82px]" alt="Cross MU Online" width={82} height={24} />
+      <Image src="/logo-icon-small.png" alt="" width={37} height={48} />
+      <Image
+        src="/logo-text.svg"
+        className="h-[24px] w-[82px]"
+        alt="Cross MU Online"
+        width={82}
+        height={24}
+      />
     </div>
   )
 }
