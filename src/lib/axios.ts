dd1c3cdffeacc9418a971d7b3aac5f54f7b692e6
Paulo Trajano <trajanopaulo10@gmail.com>
2026-05-05 23:22:45 +0800

    fix deployment

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,15 +1,11 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-const key = process.env.YOUTUBE_API_KEY
-const baseURL = 'https://www.googleapis.com/youtube/v3'
+// const key = process.env.YOUTUBE_API_KEY ?? ''
+// const baseURL = 'https://www.googleapis.com/youtube/v3'
 
-if (!key) {
-  throw new Error('YOUTUBE_API_KEY environment variable is not set')
-}
-
-export default axios.create({
-  baseURL,
-  params: {
-    key,
-  },
-})
+// export default axios.create({
+//   baseURL,
+//   params: {
+//     key,
+//   },
+// })
